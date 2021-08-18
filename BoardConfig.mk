7957220fd89383635fa05f38699ec4d2ea0e0e37
alibei <sebastiankotzias83@gmail.com>
2021-08-18 20:46:44 +0200

    tucana: Import camera props from stock

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,6 +51,7 @@ TARGET_COPY_OUT_PRODUCT := product
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
